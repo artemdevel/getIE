3234c11f6ec38ff5c0f02d141aa5ff27f90823da
artemdevel <artem.devel2014@gmail.com>
2016-03-09 16:26:02 +0200

    Added build_all script, added .travis.yml

---
+++ build_all.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+GOOS=linux GOARCH=amd64 go build \
+    -o .build/getIE \
+    -ldflags "-X main.BuildRev=$(git rev-parse --short HEAD)" \
+    main.go
+
+GOOS=windows GOARCH=amd64 go build \
+    -o .build/getIE.exe \
+    -ldflags "-X main.BuildRev=$(git rev-parse --short HEAD)" \
+    main.go
+
+GOOS=darwin GOARCH=amd64 go build \
+    -o .build/getIE.app \
+    -ldflags "-X main.BuildRev=$(git rev-parse --short HEAD)" \
+    main.go
